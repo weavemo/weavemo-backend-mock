d27c64c9f6c44940a039019a9e78d2ba6544ab7a
weavemo <weavemo@gmail.com>
2025-12-11 13:57:49 -0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,7 @@
 
+class User:
+    def __init__(self, id, email, password, nickname):
+        self.id = id
+        self.email = email
+        self.password = password
+        self.nickname = nickname
