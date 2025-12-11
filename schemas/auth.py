77dd3aa53500a0d87837c3a4c682bdeb3503786e
weavemo <weavemo@gmail.com>
2025-12-11 13:57:10 -0700

    Update auth.py

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,1 +1,16 @@
 
+from pydantic import BaseModel
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    nickname: str
+
+class AuthResponse(BaseModel):
+    user: dict
+    token: str
+    refreshToken: str
