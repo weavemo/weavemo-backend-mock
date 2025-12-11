a0ab5f9d1fdd7997a9f03260bce9d5d2d0c6aafd
weavemo <weavemo@gmail.com>
2025-12-11 13:58:30 -0700

    Update user.py

--- routers/user.py
+++ routers/user.py
@@ -1,1 +1,18 @@
+from fastapi import APIRouter
+from db.fake_db import fake_users
+
+router = APIRouter()
+
+@router.get("/profile")
+def profile():
+    # 그냥 첫 유저 반환 (mock)
+    for email, user in fake_users.items():
+        return {
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "nickname": user.nickname
+            }
+        }
+    return {"user": None}
 
