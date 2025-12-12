69155f4102a7ad938865d34aae722cee9bddabe3
weavemo <weavemo@gmail.com>
2025-12-11 18:10:06 -0700

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET: str = "CHANGE_ME"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
