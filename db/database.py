a0ca5b9e709ceb8109028b003d14ac30a20caccb
weavemo <weavemo@gmail.com>
2025-12-12 10:33:15 -0700

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 # db/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from core.config import settings  # Settings 객체
+from config.settings import settings  # Settings 객체
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 
