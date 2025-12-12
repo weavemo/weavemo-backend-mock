6ff0ca6364482163590076574c68aab484c02475
weavemo <weavemo@gmail.com>
2025-12-12 12:10:07 -0700

    Delete models/user.py

--- models/user.py
+++
@@ -1,7 +0,0 @@
-
-class User:
-    def __init__(self, id, email, password, nickname):
-        self.id = id
-        self.email = email
-        self.password = password
-        self.nickname = nickname
