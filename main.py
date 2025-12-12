0841f78056da449784d9461487ef19b96d95b3a4
weavemo <weavemo@gmail.com>
2025-12-12 13:37:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, user
-from db.database import SessionLocal, Base, engine
 
 app = FastAPI()
-
-# db 설정
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 # ⭐ CORS 설정
 app.add_middleware(
@@ -26,6 +16,4 @@ app.add_middleware(
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user")
 
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
+
