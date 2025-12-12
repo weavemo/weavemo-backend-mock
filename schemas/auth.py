b0ce4f3332f428389eaf3d1be9831b629300a845
weavemo <weavemo@gmail.com>
2025-12-12 16:32:59 -0700

    Update auth.py

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,5 +1,6 @@
 
 from pydantic import BaseModel
+from typing import Optional
 
 class LoginRequest(BaseModel):
     email: str
@@ -14,4 +15,4 @@ class AuthResponse(BaseModel):
     user: dict
     token: str
     epiresIn: int
-    refreshToken: str | None = None
+    refreshToken: Optional[str] = None
