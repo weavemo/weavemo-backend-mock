d24e39081847bc24de499c7998e12bd2f5c947dc
weavemo <weavemo@gmail.com>
2025-12-12 16:22:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ app.add_middleware(
 )
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-app.include_router(user.router, prefix="/user", tags=["Auth"])
+app.include_router(user.router, prefix="/user", tags=["Users"])
 
 
